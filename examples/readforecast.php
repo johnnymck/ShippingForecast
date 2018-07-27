1ee06f7378f00f9059e95730d9daaecea231cf24
Johnny McKenzie <jzmckenzie@dundee.ac.uk>
2018-07-28 00:49:15 +0100

    added example readforecast

---
+++ examples/readforecast.php
@@ -0,0 +1,14 @@
+<?php
+
+include_once 'vendor/autoload.php';
+
+use johnnymck\ShippingForecast;
+
+foreach ((new ShippingForecast())->getAll()['content'] as $zone) {
+    print_r($zone['location'] . ":\n");
+    print_r($zone['wind'] . "\n");
+    print_r($zone['seas'] . "\n");
+    print_r($zone['weather'] . "\n");
+    print_r($zone['visability'] . "\n");
+    print_r("\n");
+}
